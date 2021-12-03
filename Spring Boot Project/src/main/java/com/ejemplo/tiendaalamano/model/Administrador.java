3e42598792ebc9a60fae09dc2b63391702e2a2f3
Jonnathan777 <90125734+Jonnathan777@users.noreply.github.com>
2021-12-03 16:26:46 -0500

    se crean el resto de clases en models

---
+++ Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/model/Administrador.java
@@ -0,0 +1,74 @@
+package com.ejemplo.tiendaalamano.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="usuario")
+public class Administrador implements Serializable{
+	private static final long serialVersionUID = -5773962493781143007L;
+    
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    @Column(length = 25, nullable=false)
+	private String nombre;
+
+    @Column(length = 25, nullable=false)
+	private String password;
+
+    @Column(length = 25, nullable=false)
+	private String correo;
+
+    public Administrador() {
+    }
+
+    public Administrador(Long id, String nombre, String password, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+        this.correo = correo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+}
---
+++ Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/model/Domiciliario.java
@@ -0,0 +1,63 @@
+package com.ejemplo.tiendaalamano.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="usuario")
+public class Domiciliario implements Serializable{
+	private static final long serialVersionUID = -5773962493781143007L;
+    
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    @Column(length = 25, nullable=false)
+	private String nombre;
+
+    @Column(length = 25, nullable=false)
+	private String telefono;
+
+    public Domiciliario(Long id, String nombre, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public Domiciliario(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
