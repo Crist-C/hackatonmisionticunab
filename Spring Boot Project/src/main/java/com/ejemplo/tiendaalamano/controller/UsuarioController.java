edd0ee78fc55028e765ecd0e3f35f42a174bfd14
Crist-C <castro.cristiank@gmail.com>
2021-12-04 10:07:57 -0500

    Se realiza la estructura de carpetas, se logra la primera inserción de datos a la DB.

--- Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/controller/UsuarioController.java
+++ Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/controller/UsuarioController.java
@@ -1,8 +1,17 @@
 package com.ejemplo.tiendaalamano.controller;
 
-import com.ejemplo.tiendaalamano.model.Usuario.Usuario;
+import java.util.List;
+
+import com.ejemplo.tiendaalamano.entities.Usuario;
+import com.ejemplo.tiendaalamano.model.Usuario.UsuarioCrearDto;
+import com.ejemplo.tiendaalamano.model.Usuario.UsuarioDto;
+import com.ejemplo.tiendaalamano.model.Usuario.UsuarioRegistrarRequest;
+import com.ejemplo.tiendaalamano.model.Usuario.UsuarioRest;
+import com.ejemplo.tiendaalamano.service.IUsuarioService;
 import com.ejemplo.tiendaalamano.service.UsuarioServiceImpl;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,20 +22,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController{
+
+    @Autowired
+    ModelMapper modelMapper;    //Mapeo de datos
+
+    @Autowired
+    IUsuarioService iUsuarioService; // Interfaz
     
     UsuarioServiceImpl usuarioServiceiImpl;
 
     @GetMapping
     public String obtenerUsuario() {
         
-        return "Obtener Usuario";
+        return "Obtener Usuarios";
     }
     
     @PostMapping
-    public Usuario crearUsuario(@RequestBody Usuario usuario) {
-        return usuarioServiceiImpl.save(usuario);
+    public UsuarioRest crearUsuario(@RequestBody UsuarioRegistrarRequest usuarioRegistrarRequest){
+        
+        // Toma los datos desde el formulario y los mapea a usuarioCrearDTO
+        UsuarioCrearDto usuarioCrearDto= modelMapper.map(usuarioRegistrarRequest, UsuarioCrearDto.class);
+
+        // Toma los datos desde DTO y los transforma a la entidad UsuarioDTO
+        UsuarioDto usuarioDto= iUsuarioService.crearUsuario(usuarioCrearDto);
+
+        // Mapea desde usuarioDTO al UsuarioRestModel (respuesta al usuario)
+        UsuarioRest usuarioRest= modelMapper.map(usuarioDto, UsuarioRest.class);
+
+        return usuarioRest;
     }
     
-
 
 }
